91461a7329c3b00305fbf0ac99d68d4dbbe30b74
nafissa-create <Wendpouire.Ouedraogo@strathmore.edu>
2025-06-07 11:01:52 +0300

    changes

--- practice1.py
+++ practice1.py
@@ -1,17 +1,19 @@
 import streamlit as st
-st.title("Title: Salary predictor")
+st.title("Salary predictor")
+# This line of code displays Salary predictor in a title format
 st.write("Hi, welcome...do you want to know your estimated monthly salary? Let's start😊🤗")
-
+# This line of code write arguments to our app
 first_name = st.text_input("First Name")
+st.badge(first_name)
 last_name = st.text_input("Last name")
 gender = st.selectbox("Gender", ["Male", "Female"])
-age = st.number_input("Your age", 0, 100, 30, 1)
+age = st.number_input("Your age", 15, 80, 30, 1)
 date_of_birth = st.date_input("your bithday")
-marital_status = st.radio("Marital status", ["Single", "Married"])
+marital_status = st.radio("Marital status", ["Single", "Married" ,"Divorced"])
 years_of_experience = st.slider("Years of experience", 0, 40)
 years_of_study = st.number_input("Years of study", 6, 20)
 level_of_studies = st.selectbox("Level of studies", ["High school degree", "Bachelor degree", "Master degree", "PHD degree"])
-number_of_certification = st.selectbox("Nummber of certicates", ["1","2", "3", "4", "5"])
+number_of_certification = st.selectbox("Nummber of certicates", ["1","2", "3", "4", "5" ,"+5"])
 work_domain = st.selectbox("Work domain", ["Information technology/Computing", "Healthcare/Medicine/Nursing", "Education/Teaching", "Engineering/Industry/Constuction", "Finance/Baking/Insurance", "Sales/Marketing/Commerce", "Administration/Management/Human Ressources", "Arts/Culture/Media/Communication", "Transportation/Logistics", "Hospitality/Food service/Tourism", "Agriculture/Agri-food", "Law/Legal service", "Research/Sciences", "Social service/social work", "Public administration/Government", "International trade/Export-Import", "Energy/Environment", "Sport/Recreation/Entertainment", "Real Estate", "Craftsmanship/Manufacturing/Maintenance", "Telecommunication", "Multimedia/Design/Graphic arts", "Pharmaceutical/Biotechnology", "Security/Defense/Police", "Fashion/Textile/Luxury Goods"])
 job_levels = st.selectbox("Job level", ["Intern", "Assistant", "Junior", "Intermediate/Analyst/Specialist", "Senior"])
 work_sectors = st.selectbox("Work sector", ["Public/Government sector", "NGOs/Internation Organizations", "Private companies", "Research Institutes/Universities", "Health sector","Techonology sector", "Industrial sector", "Freelance/Independent", "Informal/Community sector"])
